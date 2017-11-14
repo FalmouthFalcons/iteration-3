c27cf427e9e70c70e3cd6f0e948cd2f03066e3c4
Malcolm Ford <malcolmf38@gmail.com>
2017-11-14 14:44:04 -0600

    working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114200454) do
+ActiveRecord::Schema.define(version: 20171114200202) do
 
   create_table "order_products", force: :cascade do |t|
     t.integer "product_id"
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 20171114200454) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
-    t.string "category"
+    t.string "catergory"
     t.string "type"
-    t.float "price"
-    t.boolean "delivery"
+    t.integer "price"
+    t.boolean "delevery"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
