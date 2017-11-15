d62b1885515e360948ec5cb350e3beb1b8beba8c
Jasmine Quach <xjasminequach@gmail.com>
2017-11-14 19:10:12 -0600

    WIP ticket 13 more progress

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,31 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = products.all
+  end
+
+  def
+
+  end
+
+  def
+
+  end
+
+  def alert
+    @products = Products.find(params[:id])
+    @articles.alert
+    redirect_to products_path
+  end
 end
+
+
+
+# Given a user is authenticated
+# And performs a gesture on the Sell a product hyperlink
+# When the product form is rendered
+# Then there should be a dropdown that displays all product categories
+
+# Given a user has filled out the product form, but not chosen a category
+# When the user clicks on the Sell button
+# Then the user should be alerted to select a product category
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :title
+  validates :title, presence: true,
+                    length: {minimum: 5 }
 end
 
 # Add a new model named ProductType.
@@ -8,13 +11,3 @@ end
 # It must be non-nullable, which means that you will likely need to provide a default value for your migration to work.
 
 # The only fields needed are the primary key, and Label.
-
-
-# Given a user is authenticated
-# And performs a gesture on the Sell a product hyperlink
-# When the product form is rendered
-# Then there should be a dropdown that displays all product categories
-
-# Given a user has filled out the product form, but not chosen a category
-# When the user clicks on the Sell button
-# Then the user should be alerted to select a product category
