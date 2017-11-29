80828172ce5eb96c24e4c33783156030472cd2a6
Adam D. White <adamwhite82@gmail.com>
2017-11-29 15:24:20 -0600

    group work today during  code review

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -7,13 +7,9 @@ class ProductTypesController < ApplicationController
         # This might work
         @product_types = ProductType.all
         @products_count = Product.all.group(:product_type_id).count
-        puts @products_count
-        puts @product_types
-        @first_three = []
-        @category_products = Product.all.group(:product_type_id).limit(3)
+ 
         # @category_products = @product_types
-        puts @category_products
-        @first_three.push(@category_products)
+        
         # @product_total = Product.all(:product_type_id)
         # @product_number = ProductType.count(:category)
         # Model.distinct.pluck(:rating)
@@ -28,12 +24,6 @@ class ProductTypesController < ApplicationController
         @product_types = ProductType.new
     end
 
-     # method for category listings
-    def categoryshow
-        @products = Products.all
-        @categories = @products.product_type_id
-
-    end
 
     private
         def product_params
